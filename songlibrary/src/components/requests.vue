3b722ed79f4216ead82a64c2a2e770baf9f3f824
Altyptico <54339617+Altyptico@users.noreply.github.com>
2020-02-25 15:33:20 -0500

    Input field
    song selection field will go blank once song is submitted (entered).

--- songlibrary/src/components/requests.vue
+++ songlibrary/src/components/requests.vue
@@ -3,30 +3,44 @@
   <div>
       <ul id="requests">
         <li><button v-on:click="toggleInput">Enter Song Selection</button></li>
-        <li><input type="text" placeholder="Enter Song or Artist Name" v-show="isFieldShowing"></li>
+        <li><input v-model="song" v-on:keypress.enter="addSong" type="text" placeholder="Enter Song or Artist Name" v-show="isFieldShowing" ></li>
+        <!-- <li><button v-on:keypresss.enter="$emit('addSong')"></button></li> -->
         <li><button>Vote on Songs</button></li>
         <li><button>Bar</button></li>
       </ul>
-        
+        <p>{{song}}</p>
   </div>
 </template>
 
 <script>
 export default {
     props: {
-         isFieldShowing: {
+         isFieldShowing: {                 //error message: avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders. Use a data or computed property based on the prop's value.//
              type: Boolean,
-             default: true
+             default: false
+         },
+         song:{
+             type: String,
+             default: ""
+         },
+         songLibrary:{
+            type: Array,
+            default: function(){ return []}
          }
         
     },
    data() {
-          
+      
     },
     methods: {
         toggleInput: function (){
             this.isFieldShowing = !this.isFieldShowing; //Conditionally shows the input field when "Enter Song Selection" button is clicked//
 
+        },
+        addSong: function(){
+                this.songLibrary.push();
+                console.log(this.songLibrary);
+                this.song = '';
         }
 
     }
@@ -60,5 +74,10 @@ export default {
         font-family:'Francois One', 'sans-serif';
         font-size: 18px;
         font-weight: bold;
+    }
+    input{
+        display: inline-block;
+        margin-top: 50px;
+    
     }
 </style>
